2ede7fb65680f30fe60b8bcfe8aeb72bee8ebde6
Paulo <pjunior954@gmail.com>
2024-09-12 15:52:57 -0300

    funcoes

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,12 @@
-def mensagem_nome(nome):
+'''def mensagem_nome(nome):
     print(f"Olá {nome}")
 
-mensagem_nome(nome = "Paulo")
+mensagem_nome(nome = "Paulo") '''
+
+def sucessor_ant(numero):
+    sucessor = numero + 1
+    antecessor = numero - 1
+    return sucessor, antecessor
+
+print(sucessor_ant(15))
+
